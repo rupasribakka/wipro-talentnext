c8acdad0bd68f086bc6c8048e1671a9ccfc93acb
rupa2006 <ammulurupa674@gmail.com>
2026-07-06 23:42:20 +0530

    Added LC1 Activities

---
+++ Logic_Building/src/LC1_Number_based_problems/Activity1.java
@@ -0,0 +1,21 @@
+package LC1_Number_based_problems;
+
+public class Activity1 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int temp = a;
+
+        a = c;
+        c = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
