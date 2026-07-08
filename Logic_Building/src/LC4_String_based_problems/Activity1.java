8579afcad07ace063d1cbf13ab63d0edf4a75e31
rupa2006 <ammulurupa674@gmail.com>
2026-07-08 16:33:35 +0530

    Completed Logic Building upto LC5

---
+++ Logic_Building/src/LC4_String_based_problems/Activity1.java
@@ -0,0 +1,24 @@
+package LC4_String_based_problems;
+
+import java.util.Scanner;
+
+public class Activity1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        System.out.println("Reversed String: " + reverse);
+
+        sc.close();
+    }
+}
