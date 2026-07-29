b0cf1f29cbbf7e41716ac37a42d6ac5a80dfd8d2
rupa2006 <ammulurupa674@gmail.com>
2026-07-29 13:42:55 +0530

    added jdbc assignments

---
+++ RDBMS_SQL_JDBC/src/EstablishingConnection/Assignment1.java
@@ -0,0 +1,29 @@
+package EstablishingConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+
+        try {
+          
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XEPDB1", "hr", "hr");
+            if (con != null) {
+                System.out.println("Connection Established successfully");
+            }
+        
+            con.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Connection could not be established");
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+
+}
---
+++ RDBMS_SQL_JDBC/src/EstablishingConnection/Assignment2.java
@@ -0,0 +1,33 @@
+package EstablishingConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Assignment2 {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+        String username = "hr";
+        String password = "hr";
+
+        try {
+
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            if (con != null) {
+                System.out.println("Connection Established successfully");
+            }
+
+            con.close();
+
+        } catch (Exception e) {
+
+            System.out.println("Connection could not be established");
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+
+}
